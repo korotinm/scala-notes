e54ab5351f7d553101ad843194b095077b0b9bd7
Mikhail Korotin <korotinm@gmail.com>
2021-03-16 23:08:42 +0300

    aux

---
+++ experiments/src/main/scala/my/experiments/aux/Main.scala
@@ -0,0 +1,65 @@
+package my.experiments.aux
+
+object Main extends App{
+
+  import Foo._
+
+  trait Foo[A] {
+    type B
+    def value: B
+  }
+
+
+  trait Converter[A, R] {
+    type B = R
+    def convert(v: A): B
+  }
+
+  object Foo {
+    type Aux[A0, B0] = Foo[A0] {type B = B0}
+    type AuxConverter[A0, B0] = Converter[A0, B0]
+
+    implicit def fi = new Foo[Int] {
+      type B = String
+      def value = "int"
+    }
+
+    implicit def fl = new Foo[Long] {
+      type B = String
+      def value = "long"
+    }
+
+    implicit def fs = new Foo[String] {
+      type B = Boolean
+      def value = true
+    }
+
+    // converter
+    implicit def  int2string = new Converter[Int, String] {
+      //type B = String
+      def convert(v: Int): String = v.toString()
+    }
+
+    implicit def  int2boolean = new Converter[Int, Boolean] {
+      //type B = Boolean
+      def convert(v: Int): Boolean = if(v <= 0) false else true
+    }
+  }
+
+  def test[T, R](t: T)(implicit aux: Aux[T, R]): R = aux.value
+
+  def convert[T, R](t: T)(implicit converter: AuxConverter[T, R]): R = converter.convert(t)
+
+  val res = test(10L)
+  println(s"res: ${res}")
+
+  val res2 = test("string")
+  println(s"res2: ${res2}")
+
+  // test converter
+  val converterRes: String = convert[Int, String](10)
+  println(s"converterRes: $converterRes")
+  val converterRes2: Boolean = convert[Int, Boolean](10)
+  println(s"converterRes2: $converterRes2")
+  
+}
